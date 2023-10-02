6a042d3c337d98d0c710cf4f1863445112f2ffd2
sepehr <sep7696@gmail.com>
2023-10-02 15:24:16 +0330

    Test Commit Graph

--- BikeStores.MSSQL/BikeStore.Application/Staffs/IStaffsRepository.cs
+++ BikeStores.MSSQL/BikeStore.Application/Staffs/IStaffsRepository.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace BikeStore.Application.Staffs
-{//Test
+{//Test2
     public interface IStaffsRepository : IServiceRespository<Staff>
     {
     }
